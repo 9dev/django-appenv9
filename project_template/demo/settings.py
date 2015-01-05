3f992d4651908010a637cad1fd8c4299eff853df
9dev <9devmail@gmail.com>
2015-01-05 09:18:30 +0100

    Added media and static settings

--- project_template/demo/settings.py
+++ project_template/demo/settings.py
@@ -58,4 +58,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
