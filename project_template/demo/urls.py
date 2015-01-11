dc6b7585d4e5cb814d9a98cab2118e679477bc87
9dev <9devmail@gmail.com>
2015-01-11 21:42:58 +0100

    Updated urls

--- project_template/demo/urls.py
+++ project_template/demo/urls.py
@@ -8,3 +8,8 @@ urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^', include('main.urls', namespace='main')),
 )
+
+if settings.DEBUG:
+	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
